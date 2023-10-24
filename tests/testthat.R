03d6e5cc80aefcb964de51bef07a5c28a46360b9
erikasm5 <erikas.mikuzis@gmail.com>
2023-10-24 12:37:17 +0000

    exercise 1

---
+++ tests/testthat.R
@@ -0,0 +1,33 @@
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where should you do additional test configuration?
+# Learn more about the roles of various files in:
+# * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
+# * https://testthat.r-lib.org/articles/special-files.html
+
+library(testthat)
+library(group22)
+
+test_that("randomDNAseq returns sequences of correct length", {
+  len <- 50
+  seq <- randomDNAseq(len)
+  expect_equal(nchar(seq), len)  # Use nchar() instead of length()
+})
+
+test_that("randomDNAseq returns only valid DNA bases", {
+  len <- 100
+  seq <- randomDNAseq(len)
+  valid_bases <- c("A", "T", "G", "C")
+  all_valid <- all(strsplit(seq, NULL)[[1]] %in% valid_bases)
+  expect_true(all_valid)
+})
+
+test_that("randomDNAseq returns a roughly even distribution of bases", {
+  len <- 1e5
+  seq <- randomDNAseq(len)
+  freq <- table(strsplit(seq, NULL)[[1]])
+  # Here, we're checking if any base occurs less than 20% or more than 30% of the time.
+  # Adjust these numbers as appropriate for your desired level of "evenness".
+  expect_true(all(freq >= 0.2 * len & freq <= 0.3 * len))
+})
